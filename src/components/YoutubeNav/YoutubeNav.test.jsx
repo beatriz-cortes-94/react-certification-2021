f03a000329d2d0c4998cfacb22e029475f57323d
Betty Cortés <bettycortes@Bettys-MacBook-Pro.local>
2021-07-26 12:37:56 -0500

    adding tests for homeview

---
+++ src/components/YoutubeNav/YoutubeNav.test.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Nav from './YoutubeNav.component';
+
+describe('Nav elements', () => {
+  test('must display navbar', () => {
+    render(<Nav />);
+    const icons = screen.getAllByRole('navigation');
+    expect(icons).toHaveLength(1);
+  });
+  test('must display hamburger menu', () => {
+    render(<Nav />);
+    const hamburgerMenu = screen.queryAllByRole('button');
+    expect(hamburgerMenu).toHaveLength(1);
+  });
+  test('must display search bar', () => {
+    render(<Nav />);
+    const searchBar = screen.getByPlaceholderText('Search...');
+    expect(searchBar).toBeInTheDocument();
+  });
+});
